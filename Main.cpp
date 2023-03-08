2f8ddb75624da86c9bfbc17067a64107d8a8f9f7
Ben McCormack <g00416547@atu.ie>
2023-03-08 15:09:24 +0000

    Tested The Random Function

--- Main.cpp
+++ Main.cpp
@@ -7,7 +7,22 @@
 
 int main()
 {
+	int board[3][3];
+	int random, i, j;
+	const int RANGE = 2;
 
+	srand(time(0));
+	random = rand();
+
+//for (i = 0; i < 3; i++)
+//{
+//	for (j = 0; j < 3; j++)
+//		{
+//			random = rand() % RANGE;
+//			printf("%i  ", random);
+//		}
+//		printf("\n");
+//	}
 
 	return 0;
 }

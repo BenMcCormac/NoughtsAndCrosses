a3563c11f24c070f77a9c1b54669eb20fe4a9937
RUPERT CLAISSE - STUDENT <g00423530@atu.ie>
2023-03-08 15:41:58 +0000

    Created wincheck function (including its prototype) with nested for loops to check each row, column, as well as one of the two diagonals

--- Main.cpp
+++ Main.cpp
@@ -5,9 +5,43 @@
 #include <stdlib.h>
 #include <math.h>
 
+void wincheck(int grid[3][3]);
+
 int main()
 {
 
 
 	return 0;
+}
+
+void wincheck(int grid[3][3]) {
+	// Call this function with wincheck(board); in int main()
+	int i, j, k; // i is the variable for rows, j is the variable for columns, k is the variable for checking the diagonals
+
+	for (i = 0; i < 2; i++) // Checks each row
+	{
+		for (j = 0; j < 1; j++)
+		{
+			if (grid[i][j] != grid[i][j + 1])
+				break;
+			printf("Player won"); // Should modify printf statements in all three nested for loops to indicate if computer or player won
+		}
+	}
+
+	for (j = 0; j < 2; j++) // Checks each column
+	{
+		for (i = 0; i < 1; i++)
+		{
+			if (grid[i][j] != grid[i + 1][j])
+				break;
+			printf("Player won");
+		}
+	}
+
+	for (k = 0; k < 1; k++)
+	{
+		if (grid[k][k] != grid[k + 1][k + 1])
+			break;
+		printf("Player won");
+	}
 }

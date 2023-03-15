2e035ed34631f28c28e204dd895d9cfbd8f0ce48
Rupert <g00423530@atu.ie>
2023-03-15 15:59:00 +0000

    Added a way to check if a player has won

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 void wincheck(char grid[3][3]);
 
@@ -17,14 +18,17 @@ int main()
 void wincheck(char grid[3][3]) {
 	// Call this function with wincheck(board); in int main()
 	int i, j, k, l; // i is the variable for rows, j is the variable for columns, k is the variable for checking the first diagonal, l is the variable for checking the second diagonal
-
+	char human[15] = "";
 	for (i = 0; i <= 2; i++) // Checks each row
 	{
 		for (j = 0; j < 2; j++)
 		{
 			if (grid[i][j] != grid[i][j + 1])
 				break;
-			printf("Player won with row number %i", i + 1); // Should modify printf statements in all three nested for loops to indicate if computer or player won
+			else if (grid[i] == "X" || grid[i] == "x")
+				printf("Computer won with row number %i", i + 1); // Should modify printf statements in all three nested for loops to indicate if computer or player won
+			else if (grid[i] == "O" || grid[i] == "o")
+				printf("%c won with row number %i", human[15], i + 1);
 		}
 	}
 
@@ -34,7 +38,10 @@ void wincheck(char grid[3][3]) {
 		{
 			if (grid[i][j] != grid[i + 1][j])
 				break;
-			printf("Player won with column number %i", j + 1);
+			else if (grid[j] == "X" || grid[j] == "x")
+				printf("Computer won with column number %i", j + 1); // Should modify printf statements in all three nested for loops to indicate if computer or player won
+			else if (grid[j] == "O" || grid[j] == "o")
+				printf("%c won with column number %i", human[15], j + 1);
 		}
 	}
 
@@ -42,13 +49,19 @@ void wincheck(char grid[3][3]) {
 	{
 		if (grid[k][k] != grid[k + 1][k + 1])
 			break;
-		printf("Player won with descending diagonal from left to right");
+		else if (grid[k] == "X" || grid[k] == "x")
+			printf("Computer won with descending diagonal from left to right"); // Should modify printf statements in all three nested for loops to indicate if computer or player won
+		else if (grid[k] == "O" || grid[k] == "o")
+			printf("%c won with descending diagonal from left to right", human[15]);
 	}
 
 	for (l = 2; l >= 1; l--)
 	{
 		if (grid[l][l] != grid[l - 1][l - 1])
 			break;
-		printf("Player won with ascending diagonal from left to right");
+		else if (grid[l] == "X" || grid[l] == "x")
+			printf("Computer won with ascending diagonal from left to right"); // Should modify printf statements in all three nested for loops to indicate if computer or player won
+		else if (grid[l] == "O" || grid[l] == "o")
+			printf("%c won with ascending diagonal from left to right", human[15]);
 	}
 }

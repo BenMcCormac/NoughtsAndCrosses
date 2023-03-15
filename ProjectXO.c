17398fd113e4a303bee9645ae4a250eee76e75ff
ATUstudent <118981725+ATUstudent@users.noreply.github.com>
2023-03-15 14:53:25 +0000

    Add files via upload
    Updated player choice and turn

--- ProjectXO.c
+++ ProjectXO.c
@@ -16,45 +16,48 @@ int main()
 
     {
         player = (player % 2) ? 1 : 2;
-
+            
         printf("Player %d,", player);
         scanf_s("%d", &choice);
 
         mark = (player == 1) ? 'X' : 'O';
 
-        if (choice == 1 && grid[1] == '1')
-            grid[1] = mark;
-
-        else if (choice == 2 && grid[2] == '2')
-            grid[2] = mark;
-
-        else if (choice == 3 && grid[3] == '3')
-            grid[3] = mark;
-
-        else if (choice == 4 && grid[4] == '4')
-            grid[4] = mark;
-
-        else if (choice == 5 && grid[5] == '5')
-            grid[5] = mark;
-
-        else if (choice == 6 && grid[6] == '6')
-            grid[6] = mark;
-
-        else if (choice == 7 && grid[7] == '7')
-            grid[7] = mark;
-
-        else if (choice == 8 && grid[8] == '8')
-            grid[8] = mark;
-
-        else if (choice == 9 && grid[9] == '9')
-            grid[9] = mark;
-
-        else
+        for (i = 0; i < 5; i++)
         {
-            printf("Incorrect move\n try again");
 
-            return(0);
+            if (choice == 1 && grid[1] == '1')
+                grid[1] = mark;
+
+            else if (choice == 2 && grid[2] == '2')
+                grid[2] = mark;
+
+            else if (choice == 3 && grid[3] == '3')
+                grid[3] = mark;
+
+            else if (choice == 4 && grid[4] == '4')
+                grid[4] = mark;
+
+            else if (choice == 5 && grid[5] == '5')
+                grid[5] = mark;
+
+            else if (choice == 6 && grid[6] == '6')
+                grid[6] = mark;
+
+            else if (choice == 7 && grid[7] == '7')
+                grid[7] = mark;
+
+            else if (choice == 8 && grid[8] == '8')
+                grid[8] = mark;
+
+            else if (choice == 9 && grid[9] == '9')
+                grid[9] = mark;
+
+            else
+            {
+                printf("Incorrect move\n try again");
+                return(0);
+            }
+            
         }
-
     }
 }

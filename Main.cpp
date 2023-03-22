d387e7c88faf69b2beb20f495bc49bf1197d7286
Ben McCormack <g00416547@atu.ie>
2023-03-22 17:53:34 +0000

    Done for the day

--- Main.cpp
+++ Main.cpp
@@ -348,11 +348,17 @@ void wincheck(char grid[3][3]) {
 		for (j = 0; j < 2; j++)
 		{
 			if (grid[i][j] != grid[i][j + 1])
+			{
 				break;
+			}
 			else if (grid[i] == "X" || grid[i] == "x")
+			{
 				printf("Computer won with row number %i", i + 1); // Should modify printf statements in all three nested for loops to indicate if computer or player won
+			}
 			else if (grid[i] == "O" || grid[i] == "o")
+			{
 				printf("%c won with row number %i", human[14], i + 1);
+			}
 		}
 	}
 
@@ -361,31 +367,48 @@ void wincheck(char grid[3][3]) {
 		for (i = 0; i < 2; i++)
 		{
 			if (grid[i][j] != grid[i + 1][j])
+			{
 				break;
+			}
 			else if (grid[j] == "X" || grid[j] == "x")
+			{
 				printf("Computer won with column number %i", j + 1); // Should modify printf statements in all three nested for loops to indicate if computer or player won
+			}
 			else if (grid[j] == "O" || grid[j] == "o")
+			{
 				printf("%c won with column number %i", human[14], j + 1);
+			}
 		}
 	}
 
 	for (k = 0; k <= 1; k++)
 	{
 		if (grid[k][k] != grid[k + 1][k + 1])
+		{
 			break;
+		}
 		else if (grid[k] == "X" || grid[k] == "x")
+		{
 			printf("Computer won with descending diagonal from left to right"); // Should modify printf statements in all three nested for loops to indicate if computer or player won
+		}
 		else if (grid[k] == "O" || grid[k] == "o")
+		{
 			printf("%c won with descending diagonal from left to right", human[14]);
+		}
 	}
 
 	for (l = 2; l >= 1; l--)
 	{
 		if (grid[l][l] != grid[l - 1][l - 1])
+		{
 			break;
+		}
 		else if (grid[l] == "X" || grid[l] == "x")
+		{
 			printf("Computer won with ascending diagonal from left to right"); // Should modify printf statements in all three nested for loops to indicate if computer or player won
-		else if (grid[l] == "O" || grid[l] == "o")
+		}
+		else if (grid[l] == "O" || grid[l] == "o") {
 			printf("%c won with ascending diagonal from left to right", human[14]);
+		}
 	}
 }
